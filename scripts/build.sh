38db284d396945b83259556bd174ba931e3d9214
Raphael Rafatpanah <persianturtle@gmail.com>
2021-09-19 22:17:30 -0400

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,5 @@ mkdir public
 cp src/index.html public
 npx rescript
 npx esbuild lib/es6/src/Index.bs.js --entry-names=[name]-[hash] --bundle --minify --outfile=public/app.js
-file=$(find public -name 'app*')
-file=$(file:7:15)
+file=$(find public -name 'app*' | sed 's/public\///')
 sed -i '' "s/<\/body>/<script src=\"\/$file\"><\/script><\/body>/" public/index.html 
